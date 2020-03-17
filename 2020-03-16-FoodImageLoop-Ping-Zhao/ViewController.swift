26191b2128da9bd53403eb31f2e92f5d6b2cc4b8
Ping Zhao <pzhao11@gmail.com>
2020-03-16 19:24:58 -0700

    Initial Commit

---
+++ 2020-03-16-FoodImageLoop-Ping-Zhao/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2020-03-16-FoodImageLoop-Ping-Zhao
+//
+//  Created by Ping Zhao on 3/16/20.
+//  Copyright © 2020 Ping Zhao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
